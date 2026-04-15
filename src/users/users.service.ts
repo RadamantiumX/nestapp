2669116c8617ff9854270cd52de8da2cbb4701b0
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2026-04-14 23:38:47 -0300

    feat: new testing docker

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from 'src/prisma.service';
+import { Prisma, User } from 'generated/prisma/client';
 
 @Injectable()
 export class UsersService {
 
    constructor(private prisma: PrismaService){}
 
-    getUsers(){
-        return this.prisma.user.findMany()
+    getUsers(params:{
+      skip?:number;
+      take?:number;
+      cursor?: Prisma.UserWhereUniqueInput;
+      where?: Prisma.UserWhereInput;
+      orderBy?: Prisma.UserOrderByWithRelationInput;
+    }):Promise<User[]>{
+       const { skip, take, cursor, where, orderBy } = params
+        return this.prisma.user.findMany({
+          skip,
+          take,
+          cursor,
+          where,
+          orderBy
+        })
     }
 
-    createUser(user: CreateUserDto){
+    createUser(data: Prisma.UserCreateInput):Promise<User>{
      
-      return this.prisma.user.create({data: user})
+      return this.prisma.user.create({ data })
     }
 }
